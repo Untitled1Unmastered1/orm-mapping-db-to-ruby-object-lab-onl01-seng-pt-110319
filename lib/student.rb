73e3fe5d9fcca02dd9492c6b8348257a8f5b5e87
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-12 06:16:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,23 @@ class Student
     end.first
   end
   
-  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql)
+  end
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT * FROM students 
+    WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+  end
     
 
   def save
