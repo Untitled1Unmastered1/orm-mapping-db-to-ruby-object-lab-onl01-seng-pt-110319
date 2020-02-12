db2f4b686296a87a8831633bf2a41e34b37c6818
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-12 06:20:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,11 +55,8 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(x)
-    sql = <<-SQL
-    SELECT * FROM students
-    WHERE grade = 10 LIMIT ?
-    SQL 
-    DB[:conn].execute(sql, x).map do |row|
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
+    DB[:conn].execute(sql, x).collect do |row|
       self.new_from_db(row) 
     end
   end
